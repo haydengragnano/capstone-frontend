518b92a60c646b37493909f9d064cb1f49fef0e9
hayden <1haydengragnano@gmail.com>
2021-11-09 11:27:24 -0800

    adds theme to user show page

--- src/views/GamesShow.vue
+++ src/views/GamesShow.vue
@@ -1,8 +1,76 @@
 <template>
   <div class="games-show">
-    Search By Tag
-    <br />
-    <input type="text" v-model="tagSearch" />
+    <div id="content">
+      <div class="container">
+        <div class="row justify-content-md-center">
+          <div class="col col-lg-12 col-xl-10">
+            <div class="row has-sidebar">
+              <div class="col-md-5 col-lg-4 col-xl-4 col-sidebar">
+                <div id="sidebar" class="sidebar-left">
+                  <div class="sidebar_inner">
+                    <div class="card shadow agent-search">
+                      <h3 class="subheadline mt0">Search Gamers</h3>
+                      <form>
+                        <div class="form-group">
+                          <label for="dealing">Tags</label>
+                          <select class="form-control ui-select" id="dealing" v-model="tagSearch">
+                            <option v-for="tag in tags" v-bind:key="tag.id">{{ tag.name }}</option>
+                          </select>
+                        </div>
+                        <button type="submit" class="btn btn-lg btn-block btn-primary">Search</button>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-7 col-lg-8 col-xl-8">
+                <div class="page-header bordered mt0">
+                  <h1>
+                    {{ game.title }}
+                    <p>
+                      <strong>All these gamers are playing {{ game.title }} right now!!!</strong>
+                    </p>
+                  </h1>
+                  <img v-bind:src="game.image_url" v-bind:alt="game.title" />
+                </div>
+                <div class="sorting">
+                  <div class="row justify-content-between">
+                    <div class="col-sm-5 col-md-5 col-lg-4 col-xl-4">
+                      <div class="form-group"></div>
+                    </div>
+                    <div class="col-sm-6 col-md-5 col-lg-4 col-xl-3"></div>
+                  </div>
+                </div>
+                <div class="clearfix"></div>
+                <div class="item-listing list">
+                  <div class="item" v-for="user in filterBy(game.users, tagSearch, 'tags')" v-bind:key="user.id">
+                    <div class="row">
+                      <div class="col-md-3">
+                        <div class="item-image">
+                          <img v-bind:src="user.image_url" v-bind:alt="user.handle" class="img-fluid" />
+                        </div>
+                      </div>
+                      <div class="col-md-9">
+                        <router-link v-bind:to="`/users/${user.id}`" class="btn btn-primary float-right">
+                          View Profile
+                        </router-link>
+                        <h3 class="item-title">
+                          <h3>{{ user.handle }}</h3>
+                        </h3>
+
+                        <div class="item-description" v-for="tag in user.tags" v-bind:key="tag.id">
+                          <p>{{ tag.name }}</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
     <!-- <div v-for="tag in tags" v-bind:key="tag.id">
       <input type="checkbox" :id="tag.id" :value="tag.name" v-model="tagSearch" />
@@ -24,6 +92,9 @@
       {{ tagSearch }}
       <pre class="language-json">{{ value }}</pre>
     </div> -->
+    <!-- Search By Tag
+    <br />
+    <input type="text" v-model="tagSearch" />
     <h1>{{ game.title }}</h1>
     <img v-bind:src="game.image_url" v-bind:alt="game.title" />
     <div v-for="user in filterBy(game.users, tagSearch, 'tags')" v-bind:key="user.id">
@@ -35,7 +106,7 @@
       <router-link v-bind:to="`/users/${user.id}`">view profile</router-link>
     </div>
     <br />
-    <router-link to="/games">Back to all games</router-link>
+    <router-link to="/games">Back to all games</router-link> -->
   </div>
 </template>
 <style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
--- src/views/UsersShow.vue
+++ src/views/UsersShow.vue
@@ -1,20 +1,97 @@
 <template>
   <div class="users-show">
-    <span v-if="verifyUser()">
-      <router-link v-bind:to="`/users/${user.id}/edit`">Edit User</router-link>
-    </span>
-    <br />
-    <h1>{{ user.handle }}</h1>
-    <a :href="`${user.stream_url}`" target="_blank">
-      <h2>{{ user.stream_url }}</h2>
-    </a>
-    <br />
-    <img v-bind:src="user.image_url" v-bind:alt="user.handle" />
-    <p>bio:{{ user.bio }}</p>
-    <p>game of choice: {{ user.game.title }}</p>
-    <router-link :to="`/games/${user.game.id}`"><img :src="user.game.image_url" /></router-link>
-    <div v-for="tag in user.tags" v-bind:key="tag.id">
-      <p>{{ tag.name }}</p>
+    <div id="content">
+      <div class="container">
+        <div class="row justify-content-md-center">
+          <div class="col col-lg-12 col-xl-10">
+            <div class="row has-sidebar">
+              <div class="col-md-5 col-lg-4 col-xl-4 col-sidebar">
+                <div id="sidebar" class="sidebar-left">
+                  <div class="sidebar_inner">
+                    <div class="text-center">
+                      <img
+                        class="img-fluid img-rounded agent-thumb"
+                        v-bind:src="user.image_url"
+                        v-bind:alt="user.handle"
+                      />
+                    </div>
+                    <h3 class="subheadline">
+                      <h1>{{ user.handle }}</h1>
+                    </h3>
+                    <span v-if="verifyUser()">
+                      <router-link
+                        v-bind:to="`/users/${user.id}/edit`"
+                        class="btn btn-lg btn-primary btn-block"
+                        data-toggle="modal"
+                        data-target="#leadform"
+                      >
+                        Edit User
+                      </router-link>
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-7 col-lg-8 col-xl-8">
+                <div class="page-header mt-0">
+                  <h1>About {{ user.handle }}</h1>
+                </div>
+                <strong>
+                  {{ user.bio }}
+                </strong>
+                <hr />
+                <div class="lead">{{ user.handle }} is playing {{ user.game.title }} right now!</div>
+                <div class="sorting">
+                  <div class="row justify-content-between">
+                    <div class="col-sm-5 col-md-5 col-lg-4 col-xl-4">
+                      <div class="form-group"></div>
+                    </div>
+                    <div class="col-sm-6 col-md-5 col-lg-4 col-xl-3"></div>
+                  </div>
+                </div>
+                <div class="clearfix"></div>
+                <div class="item-listing list">
+                  <div class="item">
+                    <div class="row">
+                      <div class="col-lg-5">
+                        <div class="item-image">
+                          <strong>{{ user.game.title }}</strong>
+                          <router-link :to="`/games/${user.game.id}`">
+                            <img :src="user.game.image_url" />
+                          </router-link>
+                        </div>
+                      </div>
+                      <div class="col-lg-7">
+                        <div class="item-info">
+                          <h2 class="item-title">
+                            <a :href="`${user.stream_url}`" target="_blank">
+                              {{ user.stream_url }}
+                            </a>
+                          </h2>
+
+                          <div class="item-details-i"></div>
+                          <div class="item-details">
+                            <ul>
+                              <li></li>
+                              <li>
+                                <strong>when playing {{ user.game.title }} I play:</strong>
+                                <br />
+                                <br />
+                                <div v-for="tag in user.tags" v-bind:key="tag.id">
+                                  <p>{{ tag.name }}</p>
+                                </div>
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
